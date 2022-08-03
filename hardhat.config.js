6f94d0a766bddf4187d548d609c2a6336c157caf
bizmindx <bizmindx@gmail.com>
2022-08-03 12:53:23 +0100

    fix final failing test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,12 @@
-require("dotenv").config();
-
-require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-waffle");
-require("hardhat-gas-reporter");
-require("solidity-coverage");
+import "dotenv/config";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "hardhat-contract-sizer";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import { HardhatUserConfig, task } from "hardhat/config";
+// import "tsconfig-paths/register";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -21,15 +24,21 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
+const config: HardhatUserConfig = {
   solidity: {
-    version: "0.6.8",
+    version: "0.8.3",
     settings: {
       optimizer: {
         enabled: true,
         runs: 200,
       },
     },
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
   },
   networks: {
     ropsten: {
@@ -39,12 +48,12 @@ module.exports = {
     },
     hardhat: {
       mining: {
-        auto: true
+        auto: true,
       },
-      // forking: {
-      //   url: "https://eth-mainnet.alchemyapi.io/v2/_BQD0Esx8ZAKb9KAB-0yX40UzodFYCmn",
-      //   // blockNumber: 14622817,
-      // },
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/ZICExelq-k-VZwFgpzolGBMjQ9sZGQJJ",
+        blockNumber: 14622817,
+      },
     },
   },
   gasReporter: {
@@ -55,3 +64,5 @@ module.exports = {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
+
+export default config;
